cc95fddf8fecc6c4cb01e2c91fe1c7bf03cac167
Juanesgiral2 <198259557+Juanesgiral2@users.noreply.github.com>
2026-03-31 22:07:01 -0500

    Declaración del modelo inscripcion

---
+++ src/models/enrollment.js
@@ -0,0 +1,35 @@
+//Importación de los módulos requeridos
+import {v4 as uuidv4} from "uuid"
+import {db} from "../config/dataBase.js"
+//Declaración de la clase EnrollmentModel
+export default class EnrollmentModel{
+    //Métodos
+    //Crear una inscripción
+    async create (data){
+        const {studentId,courseId} = data
+
+        return await db.enrollment.create({
+            data:{
+                id:uuidv4(),
+                studentId,
+                courseId
+            }
+        })
+    }
+    //Ver las inscripciones vinculados con el id del usuario activo del sistema
+    async getAll(id){
+        return await db.enrollment.findMany({
+            where:{
+                userId:id
+            }
+        })
+    }
+    //Eliminar una inscripción por el id
+    async remove (id){
+        return await db.enrollment.delete({
+            where:{
+                id
+            }
+        })
+    }
+}
