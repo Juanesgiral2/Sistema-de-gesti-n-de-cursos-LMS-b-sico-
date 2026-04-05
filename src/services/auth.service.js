77de4482217340ebff6685749a8f3644f70a057f
Juanesgiral2 <198259557+Juanesgiral2@users.noreply.github.com>
2026-04-05 09:16:24 -0500

    Declaración de la clase AuthService

---
+++ src/services/auth.service.js
@@ -0,0 +1,45 @@
+//Importación de los módulos requeridos
+import UserService from "./user.service.js"
+import jwt from "jsonwebtoken"
+import {compare} from "../utils/hash.js"
+//Declaración y exportación de la clase AuthService
+export default class AuthService{
+    //Métodos
+    //Registrar un usuario
+    static async register (data){
+        //Verificar si el usuario existe
+        const {email} = data
+        const found = await UserService.getForAttribute(email)
+
+        if(found){
+            throw new Error("El usuario ya existe")
+        }
+
+        return await UserService.createUser(data)
+    }
+    //Login
+    static async login({email,password}){
+        //Verificar que el email sea correcto
+        const userFound = await UserService.getForAttribute(email)
+
+        if(!userFound){
+            throw new Error("El email es incorrecto")
+        }
+
+        //Verificar que las contraseñas coincidan
+        const valid = await compare(password,userFound.password)
+
+        if(!valid) {
+            throw new Error("Contraseña invalida")
+        }
+
+        //Generar el token
+        const token = jwt.sign(
+            {id:userFound.id,role:userFound.role},
+            process.env.JWTSECRET,
+            {expiresIn:"1h"}
+        )
+
+        return { token, user: { id: userFound.id, role: userFound.role } }
+    }
+}
